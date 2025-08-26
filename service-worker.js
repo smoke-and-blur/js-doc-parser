80b0416383bda0184f2dd14fd6112672d10c1c97
Alex Hulak <lx.hulak@gmail.com>
2025-08-26 17:18:31 +0300

    Add a simple service worker for debugging purposes

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
-// self.addEventListener('fetch', (event) => {
-//   console.log(event)
-// });
+self.addEventListener('fetch', (event) => {
+  console.log(event)
+  event.respondWith(
+    () => {
+        return fetch(event.request)
+    }
+  )
+});
