279a9ced06eaf7ab190a170ebc6ddde61696c096
Alex Hulak <lx.hulak@gmail.com>
2025-08-29 14:53:48 +0300

    Make the SW fetch function validate the URL!

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,15 @@
 self.addEventListener('fetch', (event) => {
   console.log(event)
+  if (event.request.url.pathname !== "/") {
+    return
+  }
+
+  if (event.request.method !== "POST") {
+    return
+  }
+  
   event.respondWith(
-    async function() {
+    async () => {
         return fetch(event.request)
     }
   )
